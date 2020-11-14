ee6fa3fa33de4b1ea609c758d34edcd26771ca14
drozen1 <drozen@campus.technion.ac.il>
2020-11-14 10:53:26 +0000

    ..

---
+++ cmake-build-debug/CMakeFiles/HW1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW1.dir/Commands.cpp.o"
+  "CMakeFiles/HW1.dir/signals.cpp.o"
+  "CMakeFiles/HW1.dir/smash.cpp.o"
+  "HW1"
+  "HW1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
